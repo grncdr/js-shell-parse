7ada78ceb822f53dae3bb724857046d5cad7ba4b
Stephen Sugden <me@stephensugden.com>
2016-03-11 20:37:21 +0100

    GH pages

---
+++ playground.js
@@ -0,0 +1,18 @@
+var parse = require('shell-parse');
+
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+var error = document.getElementById('error');
+
+input.addEventListener('change', update);
+input.addEventListener('input', update); 
+
+function update() {
+  try {
+    var value = parse(input.value);
+    output.textContent = JSON.stringify(value, null, 2);
+    error.textContent = '';
+  } catch (e) {
+    error.textContent = String(e);
+  }
+}
